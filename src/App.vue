0b4728f1b91f25c57dc310c5a5e6cfaff16059f3
stepanowon <stepanowon@hotmail.com>
2020-04-02 22:58:17 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <img class="w-full" src="./assets/profile.jpg" alt="Display" />
+      <div class="px-6 py-4">
+        <div class="font-bold text-purple-500 text-xl mb-2">
+          Vue + Tailwind 템플릿
+        </div>
+        <p class="text-gray-700 text-base">
+          Vue도 좋지만 tailwind까지 결합되니 정말 좋네요. <span></span>
+        </p>
+      </div>
+      <div class="px-6 py-4">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#vue</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#@vue/cli</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mt-2 ml-20">#tailwind</span>
+      </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
